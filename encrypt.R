062d304cb9a5c1d3e26b2dfe1d799a8d247f2297
Keith Douglas <keith@keithdouglas.co.uk>
2025-04-24 15:08:25 +0100

    encrypts site

---
+++ encrypt.R
@@ -0,0 +1,11 @@
+staticryptR::staticryptr(
+  files = "docs",
+  directory = ".",
+  password = "DataScienceIsAnExcitingDiscipline",
+  recursive = TRUE,
+  template_color_primary = "#041e42",
+  template_color_secondary = "#f9f9f3",
+  template_title = "Protected Content",
+  template_instructions = "Enter the password or contact example@email.com",
+  template_button = "Access"
+)
